ece7c68fd9dc4817860f563011117fc7b103791d
Aqsa Irfan <110234498+aqsa-irfan@users.noreply.github.com>
2022-09-09 21:10:55 +0500

    Update and rename TaskOf29Aug to x_RaisedTo_y.sol

---
+++ x_RaisedTo_y.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.15;
+contract TaskOf29Aug
+{
+    
+//calculate the power
+    function power(uint x , uint y) public pure returns(uint)
+    {
+        return(x**y);
+    }
+
+}
